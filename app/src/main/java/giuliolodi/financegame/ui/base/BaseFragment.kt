a526cb554d464693716dc6069de363cbda4fa122
GLodi <lodi.giulio@gmail.com>
2017-04-03 22:28:23 +0200

    BaseFragment implemented.

---
+++ app/src/main/java/giuliolodi/financegame/ui/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package giuliolodi.financegame.ui.base
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+class BaseFragment : Fragment(), BaseContract.View {
+
+    private lateinit var mActivity: BaseActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(false)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is BaseActivity)
+            mActivity = context
+    }
+
+    override fun isNetworkAvailable(): Boolean {
+        return mActivity.isNetworkAvailable()
+    }
+
+}
