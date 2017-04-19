c2f45007a4d072a73f41f3fd3ed12faf81ec23cd
GLodi <lodi.giulio@gmail.com>
2017-04-19 22:47:13 +0200

    Improved StockActivity.

--- app/src/main/java/giuliolodi/financegame/ui/stock/StockAdapter.kt
+++ app/src/main/java/giuliolodi/financegame/ui/stock/StockAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import giuliolodi.financegame.R
 import giuliolodi.financegame.models.StockDbBought
 import kotlinx.android.synthetic.main.item_stock_activity.view.*
@@ -18,7 +19,18 @@ class StockAdapter : RecyclerView.Adapter<StockAdapter.ViewHolder>() {
         fun bind (stockDbBought: StockDbBought, currentStock: Stock) = with(itemView) {
             item_stock_activity_price.text = "$" + String.format("%.2f", stockDbBought.priceWhenBought)
             item_stock_activity_amount.text = stockDbBought.amount.toString()
-            item_stock_activity_profit.text = "$" + String.format("%.2f", currentStock.quote.price.toDouble().minus(stockDbBought.priceWhenBought!!))
+            item_stock_activity_profit.text = "Profit: $" + String.format("%.2f", currentStock.quote.price.toDouble().minus(stockDbBought.priceWhenBought!!))
+            item_stock_activity_seekbar.max = stockDbBought.amount!!
+            item_stock_activity_seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    item_stock_activity_selectedamount.text = p1.toString()
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
+
         }
     }
 
