dc2f4d370c7cc5fa4053bb0380d21bd91fdc0292
GLodi <lodi.giulio@gmail.com>
2017-05-08 19:39:44 +0200

    Updated Kotlin version and ColorUtils.

--- app/src/main/java/giuliolodi/brokersim/utils/ColorUtils.kt
+++ app/src/main/java/giuliolodi/brokersim/utils/ColorUtils.kt
@@ -1,6 +1,7 @@
 package giuliolodi.brokersim.utils
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import giuliolodi.brokersim.R
 
 class ColorUtils(context: Context) {
@@ -11,44 +12,44 @@ class ColorUtils(context: Context) {
     
     fun getRandomColor(): Int {
         val colorList: IntArray = intArrayOf(
-                mContext.resources.getColor(R.color.brown),
-                mContext.resources.getColor(R.color.deepOrange),
-                mContext.resources.getColor(R.color.orange),
-                mContext.resources.getColor(R.color.amber),
-                mContext.resources.getColor(R.color.lime),
-                mContext.resources.getColor(R.color.lightGreen),
-                mContext.resources.getColor(R.color.green),
-                mContext.resources.getColor(R.color.teal),
-                mContext.resources.getColor(R.color.cyan),
-                mContext.resources.getColor(R.color.lightBlue),
-                mContext.resources.getColor(R.color.blue),
-                mContext.resources.getColor(R.color.indigo),
-                mContext.resources.getColor(R.color.deepPurple),
-                mContext.resources.getColor(R.color.purple),
-                mContext.resources.getColor(R.color.pink),
-                mContext.resources.getColor(R.color.red))
+                ContextCompat.getColor(mContext, R.color.brown),
+                ContextCompat.getColor(mContext, R.color.deepOrange),
+                ContextCompat.getColor(mContext, R.color.orange),
+                ContextCompat.getColor(mContext, R.color.amber),
+                ContextCompat.getColor(mContext, R.color.lime),
+                ContextCompat.getColor(mContext, R.color.lightGreen),
+                ContextCompat.getColor(mContext, R.color.green),
+                ContextCompat.getColor(mContext, R.color.teal),
+                ContextCompat.getColor(mContext, R.color.cyan),
+                ContextCompat.getColor(mContext, R.color.lightBlue),
+                ContextCompat.getColor(mContext, R.color.blue),
+                ContextCompat.getColor(mContext, R.color.indigo),
+                ContextCompat.getColor(mContext, R.color.deepPurple),
+                ContextCompat.getColor(mContext, R.color.purple),
+                ContextCompat.getColor(mContext, R.color.pink),
+                ContextCompat.getColor(mContext, R.color.red))
         t = colorList[(Math.random() * colorList.size).toInt()]
         return t
     }
 
     fun getRandomDarkColor(): Int {
         when(t) {
-            mContext.resources.getColor(R.color.brown) -> return mContext.resources.getColor(R.color.brownDark)
-            mContext.resources.getColor(R.color.deepOrange) -> return mContext.resources.getColor(R.color.deepOrangeDark)
-            mContext.resources.getColor(R.color.orange) -> return mContext.resources.getColor(R.color.orangeDark)
-            mContext.resources.getColor(R.color.amber) -> return mContext.resources.getColor(R.color.amberDark)
-            mContext.resources.getColor(R.color.lime) -> return mContext.resources.getColor(R.color.limeDark)
-            mContext.resources.getColor(R.color.lightGreen) -> return mContext.resources.getColor(R.color.lightGreenDark)
-            mContext.resources.getColor(R.color.green) -> return mContext.resources.getColor(R.color.greenDark)
-            mContext.resources.getColor(R.color.teal) -> return mContext.resources.getColor(R.color.tealDark)
-            mContext.resources.getColor(R.color.cyan) -> return mContext.resources.getColor(R.color.cyanDark)
-            mContext.resources.getColor(R.color.lightBlue) -> return mContext.resources.getColor(R.color.lightBlueDark)
-            mContext.resources.getColor(R.color.blue) -> return mContext.resources.getColor(R.color.blueDark)
-            mContext.resources.getColor(R.color.indigo) -> return mContext.resources.getColor(R.color.indigoDark)
-            mContext.resources.getColor(R.color.deepPurple) -> return mContext.resources.getColor(R.color.deepPurpleDark)
-            mContext.resources.getColor(R.color.purple) -> return mContext.resources.getColor(R.color.purpleDark)
-            mContext.resources.getColor(R.color.pink) -> return mContext.resources.getColor(R.color.pinkDark)
-            mContext.resources.getColor(R.color.red) -> return mContext.resources.getColor(R.color.redDark)
+            ContextCompat.getColor(mContext, R.color.brown) -> return ContextCompat.getColor(mContext, R.color.brownDark)
+            ContextCompat.getColor(mContext, R.color.deepOrange) -> return ContextCompat.getColor(mContext, R.color.deepOrangeDark)
+            ContextCompat.getColor(mContext, R.color.orange) -> return ContextCompat.getColor(mContext, R.color.orangeDark)
+            ContextCompat.getColor(mContext, R.color.amber) -> return ContextCompat.getColor(mContext, R.color.amberDark)
+            ContextCompat.getColor(mContext, R.color.lime) -> return ContextCompat.getColor(mContext, R.color.limeDark)
+            ContextCompat.getColor(mContext, R.color.lightGreen) -> return ContextCompat.getColor(mContext, R.color.lightGreenDark)
+            ContextCompat.getColor(mContext, R.color.green) -> return ContextCompat.getColor(mContext, R.color.greenDark)
+            ContextCompat.getColor(mContext, R.color.teal) -> return ContextCompat.getColor(mContext, R.color.tealDark)
+            ContextCompat.getColor(mContext, R.color.cyan) -> return ContextCompat.getColor(mContext, R.color.cyanDark)
+            ContextCompat.getColor(mContext, R.color.lightBlue) -> return ContextCompat.getColor(mContext, R.color.lightBlueDark)
+            ContextCompat.getColor(mContext, R.color.blue) -> return ContextCompat.getColor(mContext, R.color.blueDark)
+            ContextCompat.getColor(mContext, R.color.indigo) -> return ContextCompat.getColor(mContext, R.color.indigoDark)
+            ContextCompat.getColor(mContext, R.color.deepPurple) -> return ContextCompat.getColor(mContext, R.color.deepPurpleDark)
+            ContextCompat.getColor(mContext, R.color.purple) -> return ContextCompat.getColor(mContext, R.color.purpleDark)
+            ContextCompat.getColor(mContext, R.color.pink) -> return ContextCompat.getColor(mContext, R.color.pinkDark)
+            ContextCompat.getColor(mContext, R.color.red) -> return ContextCompat.getColor(mContext, R.color.redDark)
         }
         return 0
     }
